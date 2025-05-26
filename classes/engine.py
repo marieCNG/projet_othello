6a43eb2fa9394f75f3c8428b99e835792ccbdb25
Marie Edet <marie.edet@le-campus-numerique.fr>
2025-05-26 16:53:03 +0200

    end of the day commit

--- classes/engine.py
+++ classes/engine.py
@@ -13,16 +13,56 @@ class Engine():
     def current_player(self):
         return(self._current_player)
     
+    def move_direction(sens_direction):
+
+        if sens_direction[0] == 'V':
+            if sens_direction[1] == 'H':
+                row -= 1
+            elif sens_direction[1] == 'B':
+                row += 1
+        elif sens_direction[0] == 'H':
+            if sens_direction[1] == 'G':
+                col -= 1
+            elif sens_direction[1] == 'D':
+                col += 1
+        elif sens_direction[0] == 'D':
+            if sens_direction[1:2] == 'GH':
+                row -= 1
+                col -= 1
+            elif sens_direction[1:2] == 'GB':
+                row += 1
+                col -= 1
+            elif sens_direction[1:2] == 'DH':
+                row -= 1
+                col += 1
+            elif sens_direction[1:2] == 'DB':
+                row += 1
+                col += 1
+
+        return row, col
+    
+    def find_next_pawn_diff_color(coord_dep, board, player.color):
+        directions = ['VH', 'VB', 'HG', 'HD', 'DGH', 'DGB', 'DDH', 'DDB']
+        for direction in directions:
+            row_add, col_add = Engine.move_direction(direction)
+            neighbor_pawn = board[coord_dep + row_add, coord_dep + col_add]
+            while neighbor_pawn.color != player.color:
+                row_add += row_add
+                col_add += col_add
+            
+
+
+
     def board_update(self,last_coord):
         self.othello_board.add_pawn_to_case(last_coord)
 
-        # i = row
-        # for direction in directions:
-        #     if direction[0] == 'V':
-        #         if direction[1] == 'H':
-        #             i -= 1
-        #         elif direction[1] == 'B':
-        #             i += 1
+        i = row
+        for direction in directions:
+            if direction[0] == 'V':
+                if direction[1] == 'H':
+                    i -= 1
+                elif direction[1] == 'B':
+                    i += 1
 
 
         pass # WIP, finish update by flipping Eaten pawn
