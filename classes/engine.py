2a1d6cbf16d2cbbb80637f7bb485b4ab31169f29
Thomas Mari <marithomas06@gmail.com>
2025-05-26 12:00:00 +0200

    Player v1.0

--- classes/engine.py
+++ classes/engine.py
@@ -3,14 +3,14 @@ from pawn import Pawn
 from board import Board
 
 class Engine():
-    def __init__():
+    def __init__(self):
         self.othello_board = Board()
         self.current_player = Player()
         self.next_player = Player()
     #Methods
-    def board_update():
+    def board_update(self):
         pass #WIP
-    def ask_player():
+    def ask_player(self):
         pass #WIP
-    def play():
+    def play(self):
         pass#WIP
--- classes/player.py
+++ classes/player.py
@@ -1,6 +1,33 @@
-class Player:
+# File: greetings.py
+
+class Player():
     def __init__(self,color="X"):
         self.color = color
-    def place_pawn(self):
-        coords = input("player {self.color}: Enter coordinates separated by spaces: ").split("")
-        return(list(map(lambda x:int(x),coords)))
+        self.is_autonomous = False
+        
+    def pawn_coord(self):
+        notDone = True
+        while notDone:
+            try:
+                coords_alphanum = input("player {self.color}: Enter coordinates <[A-H][1-8]>: ")
+                coord_numnum = self.convert_coord(coords_alphanum)
+                notDone = False
+            except ValueError:
+                print(f"The input {coords_alphanum} does not satisfy the syntaxe <[A-H][1-8]>, please try again")
+        return(coord_numnum)
+    def convert_coord(self,alphanumCoord:str):
+        """
+        Player.convert("A1")=(1,1) #int*int
+        Player.convert("H7")=(8,7) #int*int
+        Player.convert("----____***&&&&H7{}{}{[][]")=(8,7) #int*int
+        """
+        str_strip = alphanumCoord.strip()
+        if len(str_strip)!=2:
+            raise ValueError(f"The input {alphanumCoord} does not satisfy the syntaxe <[A-H][1-8]>")
+        x = ord(str_strip[0]) - ord('A') + 1
+        y = int(str_strip[1])
+        if x<1 or x>8:
+            raise ValueError(f"The input {alphanumCoord} does not satisfy the syntaxe <[A-H][1-8]>")  
+        if y<1 or y>8:
+            raise ValueError(f"The input {alphanumCoord} does not satisfy the syntaxe <[A-H][1-8]>")       
+        return(x,y)
