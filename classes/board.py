44b2fac721800a6abf492638b4e76cb553d5a6a2
Marie Edet <marie.edet@le-campus-numerique.fr>
2025-05-26 12:15:46 +0200

    board and pawn classes done

--- classes/board.py
+++ classes/board.py
@@ -1,14 +1,30 @@
-class Board:
-    
-    def __init__(self):
-        self.board_dict = {'11': ' ', '12': ' ', '13': ' ', '14': ' ', '15': ' ', '16': ' ', '17': ' ', '18': ' ',
-                            '21': ' ', '22': ' ', '23': ' ', '24': ' ', '25': ' ', '26': ' ', '27': ' ', '28': ' ',
-                              '31': ' ', '32': ' ', '33': ' ', '34': ' ', '35': ' ', '36': ' ', '37': ' ', '38': ' ',
-                                '41': ' ', '42': ' ', '43': ' ', '44': ' ', '45': ' ', '46': ' ', '47': ' ', '48': ' ',
-                                  '51': ' ', '52': ' ', '53': ' ', '54': ' ', '55': ' ', '56': ' ', '57': ' ', '58': ' ',
-                                    '61': ' ', '62': ' ', '63': ' ', '64': ' ', '65': ' ', '66': ' ', '67': ' ', '68': ' ',
-                                      '71': ' ', '72': ' ', '73': ' ', '74': ' ', '75': ' ', '76': ' ', '77': ' ', '78': ' ',
-                                        '81': ' ', '82': ' ', '83': ' ', '84': ' ', '85': ' ', '86': ' ', '87': ' ', '88': ' '}
+import numpy as np
+import pawn
 
-    def display(self):
-        pass
+class Board:
+    def __init__(self):
+        temp = np.array([pawn.Pawn() for i in range(64)]).reshape((8,8))
+        temp[3,3] = temp[4,4] = pawn.Pawn(color="O") # white
+        temp[3,4] = temp[4,3] = pawn.Pawn(color="X") # black
+        self.array_of_cases = temp
+
+    def add_pawn_to_case(self, coord_row, coord_col, pawn):
+        self.array_of_cases[coord_row, coord_col] = pawn
+
+    def display_board(self):
+        inter_row = "  +---+---+---+---+---+---+---+---+"
+        print("Board state is")
+        print("    A   B   C   D   E   F   G   H")
+        for i in range(8):
+            print(inter_row)
+            print("{0} | {1} | {2} | {3} | {4} | {5} | {6} | {7} | {8} |".format(i+1, self.array_of_cases[i, 0].color, self.array_of_cases[i, 1].color, self.array_of_cases[i, 2].color, self.array_of_cases[i, 3].color, self.array_of_cases[i, 4].color, self.array_of_cases[i, 5].color, self.array_of_cases[i, 6].color, self.array_of_cases[i, 7].color))
+        print(inter_row)
+
+if __name__ == "__main__":
+    board = Board()
+
+    board.display_board()
+
+    board.add_pawn_to_case(coord_row=4, coord_col=5, pawn=pawn.Pawn(color="X"))
+
+    board.display_board()
--- classes/pawn.py
+++ classes/pawn.py
@@ -1,20 +1,26 @@
 class Pawn:
-    def __init__(self, color, coord_row, coord_col):
+    def __init__(self, color=" "):
         self.color = color
-        self.coord_row = coord_row
-        self.coord_col = coord_col
 
-    def flip(self):
+    def flip_pawn(self):
         if self.color == "X":
-            self.color == "O"
+            self.color = "O"
         elif self.color == "O":
-            self.color == "X"
+            self.color = "X"
 
-    def __str__(self):
-        return f'Infos about this pawn: color is {self.color}, row coordinate is {self.coord_row}, col coordinate is {self.coord_col}'
-    
 
 if __name__ == "__main__":
 
-    pawn_1 = Pawn(color="X", coord_row=1, coord_col=1)
-    print(str(pawn_1))
+    square_11 = Pawn(color="O")
+    print(square_11.color)
+
+    square_11.flip_pawn()
+    print(square_11.color)
+
+    square_12 = Pawn(color="X")
+    print(square_12.color)
+
+    square_12.flip_pawn()
+    print(square_12.color)
+
+    print(vars(square_11))
